0a7d8718435c75581aab8e9d6377954e5cd4808e
lsz19960814 <lishangzhe@wordemotion.com>
2019-12-30 21:49:38 +0800

    singel

--- meta_stra_framwork/src/main.py
+++ meta_stra_framwork/src/main.py
@@ -11,16 +11,41 @@ from rqdata import up_file,now_file
 import quick_sig as qs
 import time
 import sig_data
-def singel_expre_test():
+import os
+single_expre_save_path = up_file+'/result/single/single.xlsx'
+
+def add_new_result(old,new):
+    if(len(old)==0):
+        for key in new.keys():
+            old[key] = []
+
+    for key in new.keys():
+        old[key].append(new[key])
+    return old
+
+def singel_expre_test(off_line = False):
     param =  params.PARAMS
     if(param['get_code_data']):
         copydata.main(param['code_list'])
-    main(_begin_date = param['begin_date'],
-        code_list = param['code_list'],
-        signal_save_path = param['_signal_save_path'],
-        Expression = param['_Expression'],
-        HS_code = param['HS_code'])
+    ori_expre,code_list = param['_Expression'],param['code_list']
+    _code_list = copy.copy(code_list)
+    if(not off_line):
+        for code in code_list:
+            if(sig_data.cal_index_data(code) == 0):
+                _code_list.remove(code)
+        HS_code = param['HS_code']
+        sig_data.cal_index_data(HS_code)
+    qs.get_signal(_code_list,ori_expre)
     results = run_func(init=init, handle_bar=handle_bar, config=param['_config'])
+    summ = results['sys_analyser']['summary']
+    summ['strategy_name'] = str(ori_expre)
+    summ['stock_num'] = len(code_list)
+    if not os.path.exists(single_expre_save_path):
+        all_re = pd.DataFrame(summ,index = [0])
+    else:
+        all_re = pd.read_excel(single_expre_save_path,index_col = 0)
+        all_re.loc[len(all_re)] = summ
+    all_re.to_excel(single_expre_save_path)
 
 def optimal_expre(off_line = False):
     param =  params.PARAMS
@@ -31,7 +56,7 @@ def optimal_expre(off_line = False):
     new_expre_list += get_new_expre.get_new_expre_list(ori_expre)
     print(new_expre_list)
     expre_result,unit_seris = {},{}
-    expre_result['expression'],expre_result['unit_net_value'] = [],[]
+    expre_result['expression'] = []
     _code_list = copy.copy(code_list)
     if(not off_line):
         for code in code_list:
@@ -40,20 +65,20 @@ def optimal_expre(off_line = False):
         HS_code = param['HS_code']
         sig_data.cal_index_data(HS_code)
     for new_expre in new_expre_list: 
-        #try:
-        start = time.clock()
-        qs.get_signal(_code_list,new_expre)
-        end = time.clock()
-        print('sig',str(end-start))
-        start = time.clock()
-        results = run_func(init=init, handle_bar=handle_bar, config=param['_config'])
-        expre_result['expression'].append(new_expre)
-        unit_seris[str(new_expre)] = results["sys_analyser"]['portfolio']['unit_net_value'].tolist()
-        expre_result['unit_net_value'].append(results["sys_analyser"]['summary']['unit_net_value'])
-        end = time.clock()
-        print('back',str(end-start))
-        #except Exception as e:
-            #print(e)
+        try:
+            start = time.clock()
+            qs.get_signal(_code_list,new_expre)
+            end = time.clock()
+            print('sig',str(end-start))
+            start = time.clock()
+            results = run_func(init=init, handle_bar=handle_bar, config=param['_config'])
+            unit_seris[str(new_expre)] = results["sys_analyser"]['portfolio']['unit_net_value'].tolist()
+            expre_result['expression'].append(new_expre)
+            expre_result = add_new_result(expre_result,results['sys_analyser']['summary'])
+            end = time.clock()
+            print('back',str(end-start))
+        except Exception as e:
+            print(e)
     unit_seris['benchmark'] = results["sys_analyser"]['benchmark_portfolio']['unit_net_value'].tolist()
     unit_seris['date'] = results["sys_analyser"]['portfolio']['unit_net_value'].index.tolist()
     expre_result_fra = pd.DataFrame(expre_result)
@@ -72,5 +97,5 @@ def bayesian_opt(expression):
     results = run_func(init=init, handle_bar=handle_bar, config=param['_config'])
     return results["sys_analyser"]['summary']['unit_net_value']
 if __name__ == "__main__":
-    optimal_expre(off_line = True)
-    #singel_expre_test()
+    #optimal_expre(off_line = True)
+    singel_expre_test()
