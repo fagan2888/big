cbafb688035f4a85e857be419c3edd82bb454f73
lsz19960814 <lishangzhe@wordemotion.com>
2020-01-16 21:09:56 +0800

    result

--- meta_stra_framwork/src/params.py
+++ meta_stra_framwork/src/params.py
@@ -19,8 +19,8 @@ sys.path.append(up_file)
 
 PARAMS = defaultdict(
 begin_date = 20150101,#信号计算开始日期
-code_list = ['600000.XSHG','000001.XSHE'],
-#code_list = get_code_list(),#信号计算的股票池
+#code_list = ['600000.XSHG','000001.XSHE'],
+code_list = get_code_list(),#信号计算的股票池
 get_code_data = False,#是否重新获得原始数据
 HS_code = '999999.XSHG',#'399300.XSHE',#信号中的大盘信号代码
 signal_lf = [1,1,1,1,1], #分别对应下面五种信号的生命周期,阈值，交叉，趋势，比较，计数
